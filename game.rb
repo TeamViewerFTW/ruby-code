5577888336a19150791a07c35026683f6c4da84d
TeamViewerFTW <atticusb05@gmail.com>
2017-09-02 13:49:45 +0800

    Updated

--- game.rb
+++ game.rb
@@ -3,7 +3,29 @@ require_relative 'level'
 require_relative 'player'
 
 class Game
-  LEVEL1 = []
+  LEVEL1 = [
+    "+------------------+",
+    "|S.|....T..........|",
+    "|..|.--------..|++.|",
+    "|..|.|....|T|..|T..|",
+    "|.+|.|.E|.|....|...|",
+    "|..|.|---.|.|--|...|",
+    "|..|.|....|.|......|",
+    "|+.|.|......|..|-|.|",
+    "|..|.|-----.|..|+|.|",
+    "|..|T.......|..|+|.|",
+    "|.++--------+..|+|.|",
+    "|.+....+++.....|+|.|",
+    "|...++.....+++.|+|.|",
+    "|---------------+..|",
+    "|T+|......|.....|.||",
+    "|..|..|......+T.|.||",
+    "|+...+|---------+..|",
+    "|..|.............+.|",
+    "T+|..+++++++++++...|",
+    "+------------------+"
+
+  ]
   def initialize(window)
     @window = window
     @player = Player.new(@window, 0, 0)
@@ -12,9 +34,27 @@ class Game
     @time_start  = time.now.to_i
   end
   def button_down(id)
+    @level.button_down(id)
   end
   def update
+    @level.update
+    if !@level.level_over?
+      @time_now = Time.now.to_i
+    end
   end
   def draw
+    @level.draw
+    draw_hud
   end
+  def draw_hud
+    if @level.level_over?
+      @font.draw("GAME OVER!", 170, 150, 10, 2, 2)
+      @font.draw("Collected #{@player.score} treasure in #{time_in_seconds} seconds!", 110, 300, 10)
+    else
+      @font.draw("Time: #{time_in_seconds}", 4, 2, 10)
+      @font.draw("Score: #{@player.score}", 510, 2, 10)
+    end
+    def time_in_seconds
+      @time_now - @time_start
+    end 
 end
