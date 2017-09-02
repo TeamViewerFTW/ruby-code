919401b23fd41c7b5443caf8083f3eabffccadfa
TeamViewerFTW <atticusb05@gmail.com>
2017-09-02 13:04:17 +0800

    1st ver, does not do anything

---
+++ amazeing.rb
@@ -0,0 +1,16 @@
+# Project 7
+# A maze.
+# Run with ruby amazeing.rb
+
+require gosu
+class Amazing < Gosu::Window
+  def initialize
+    super(640, 640)
+    self.caption = "Amazing"
+    # More code will go here
+  end
+  # Even more code will go here
+end
+
+window = Amazing.new
+window.show
